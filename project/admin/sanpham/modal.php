9b12c6e464d4066aba8a4443bf00312e41d8594a
HuyKhangPhuc <169133451+HuyKhangPhuc@users.noreply.github.com>
2024-05-28 16:06:38 +0700

    Update and rename model.php to modal.php

---
+++ project/admin/sanpham/modal.php
@@ -0,0 +1,12 @@
+<!-- Modal Structure -->
+<div id="productModal" class="modal">
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <div class="modal-body">
+      <img id="modalImg" src="" alt="Product Image">
+      <h4 id="modalTitle">Product Title</h4>
+      <p id="modalPrice">Price: $0</p>
+      <button id="addToCartBtn">Add to Cart</button>
+    </div>
+  </div>
+</div>
