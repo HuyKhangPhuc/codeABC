9bde4b5aa29d38f3cca5a130bcc49ccbcd937ad2
HuyKhangPhuc <169133451+HuyKhangPhuc@users.noreply.github.com>
2024-05-28 16:04:59 +0700

    Update shop.php

--- project/admin/sanpham/shop.php
+++ project/admin/sanpham/shop.php
@@ -336,6 +336,48 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
   </script>
   <script src="js/custom.js"></script>
+  <?php include 'modal.php'; ?>
+
+  <script>
+    // Get the modal
+    var modal = document.getElementById("productModal");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+
+    // Function to open modal with product details
+    function openModal(product) {
+      document.getElementById("modalImg").src = product.img;
+      document.getElementById("modalTitle").innerText = product.title;
+      document.getElementById("modalPrice").innerText = "Price: $" + product.price;
+      modal.style.display = "block";
+    }
+
+    // Add event listeners to product cards
+    document.querySelectorAll('.box a').forEach(function(card, index) {
+      card.addEventListener('click', function(event) {
+        event.preventDefault();
+        var product = {
+          img: card.querySelector('img').src,
+          title: card.querySelector('.detail-box h6').innerText,
+          price: card.querySelector('.detail-box span').innerText.replace('$', '')
+        };
+        openModal(product);
+      });
+    });
+  </script>
 
 </body>
 
