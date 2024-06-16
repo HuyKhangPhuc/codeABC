d94afc35bff89c6bd2b1b9a8db1ab2a809fc850a
vinhphuc <tranvanvinhphuc205@gmail.com>
2024-06-16 11:49:04 +0700

    hoàn thiện trang giới thiệu

--- project/quantri/gioithieu.php
+++ project/quantri/gioithieu.php
@@ -1,27 +1,23 @@
 <h2>giới thiệu</h2>
 <div id="main">
 <p>
-AAAAAA
+Chào mừng bạn đến với NOOB TEAM - nơi sáng tạo không ngừng nghỉ để mang đến những giải pháp quản trị đột phá. Chúng tôi tự hào giới thiệu hệ thống quản trị mới của mình, được thiết kế để cung cấp một trải nghiệm quản lý tối ưu cho người dùng.
 </p>
 <br />
 <p>
-sadasdsada
+Mục Đích: Mục tiêu của chúng tôi là tạo ra một hệ thống linh hoạt, dễ sử dụng và hiệu quả, giúp người dùng có thể quản lý thông tin một cách nhanh chóng và chính xác.
 </p>
 <br />
-<p>
 Hệ thống quản trị này có các chức năng quản lý sau:
 <br />
-- Quản lý Thành viên
+- Quản lý Thành viên: Cho phép bạn dễ dàng theo dõi và quản lý thông tin thành viên.
 <br />
-- Quản lý Danh mục sản phẩm
+- Quản lý Danh mục sản phẩm: Cung cấp công cụ để phân loại và quản lý sản phẩm một cách khoa học.
 <br />
-- Quản lý Sản phẩm
-<br />
-- ...
-</p>
+- Quản lý Sản phẩm: Tối ưu hóa việc quản lý hàng hóa, từ nhập kho đến bán hàng.
 <br />
 <p>
-Hệ thống đang trong quá trình hoàn thiện bởi các NOOB
+Tiến Độ Phát Triển: Hệ thống của chúng tôi đang trong giai đoạn hoàn thiện và chúng tôi cam kết không ngừng cải tiến để đáp ứng nhu cầu ngày càng cao của thị trường.
 </p>
 <br />
 <p>
